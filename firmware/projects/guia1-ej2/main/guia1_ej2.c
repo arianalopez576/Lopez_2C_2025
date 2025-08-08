50f957fff899b471a475299f83d5f505e868c453
Ariana <arianalopez576@gmail.com>
2025-08-08 15:31:49 -0300

    ejercicio led 3

---
+++ firmware/projects/guia1-ej2/main/guia1_ej2.c
@@ -0,0 +1,90 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+
+/*==================[end of file]============================================*/
+/*! @mainpage Blinking switch
+ *
+ * \section genDesc General Description
+ *
+ * This example makes LED_1 and LED_2 blink if SWITCH_1 or SWITCH_2 are pressed.
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 1000
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+/*modificar el codigo de blinking 1 para que al apretar la tecla 1 encienda led 1*/
+
+void app_main(void){
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas = SwitchesRead();
+		switch(teclas){
+    		case SWITCH_1:
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+    			LedToggle(LED_2);
+    		break;
+			case (SWITCH_1 | SWITCH_2):
+				LedToggle(LED_3);
+			break;
+    	}
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
+}
