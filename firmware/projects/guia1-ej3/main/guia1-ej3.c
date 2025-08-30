b9a2f0f03b4f4430ed34e95ffed63efb2257529e
Ariana <arianalopez576@gmail.com>
2025-08-29 21:06:43 -0300

    Se agrega LedOn Ej3

--- firmware/projects/guia1-ej3/main/guia1-ej3.c
+++ firmware/projects/guia1-ej3/main/guia1-ej3.c
@@ -26,6 +26,9 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h> //para utilizar true/false
+#include "freertos/FreeRTOS.h" //para funciones de tiempo real
+#include <freertos/task.h> //para las tareas asociadas a freeRTOS
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
@@ -34,6 +37,31 @@
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	struct leds
+	{
+	uint8_t mode; //ON, OFF, TOGGLE
+	uint8_t n_led; //indica el número de led a controlar
+	uint8_t n_ciclos; //indica la cantidad de ciclos de encendido/apagado (parpadeo)
+	uint16_t periodo; //indica el tiempo de cada ciclo
+	} 
 }
+	//declaro una funcion void para el control de los leds
+	//esta funcion tiene como parametro un puntero al struct de leds
+	void control_leds (struct leds *my_leds)
+	{
+		//esta funcion es la que sigue el diagrama de flujos
+		//la flecha es un operador de acceso a miembros de un puntero
+		//----MODO ON----
+		if (my_leds -> mode == ON){
+			if (my_leds-> n_led == 1){
+				LedOn(1);
+			}
+			else if (my_leds-> n_led == 2){
+				LedOn(2);
+			}
+			else if (my_leds-> n_led == 3){
+				LedOn(3);
+			}
+		}
+	}
 /*==================[end of file]============================================*/
