efc3fc949589fc60286bc252d385b0f4699eddb4
Ariana <arianalopez576@gmail.com>
2025-09-05 16:19:43 -0300

    Se agrega guia1ej3

--- firmware/projects/guia1-ej3/main/guia1-ej3.c
+++ firmware/projects/guia1-ej3/main/guia1-ej3.c
@@ -27,41 +27,84 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h> //para utilizar true/false
+#include "led.h"
+#include "switch.h"
 #include "freertos/FreeRTOS.h" //para funciones de tiempo real
 #include <freertos/task.h> //para las tareas asociadas a freeRTOS
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
-
-/*==================[internal functions declaration]=========================*/
-
-/*==================[external functions definition]==========================*/
-void app_main(void){
-	struct leds
+struct leds
 	{
 	uint8_t mode; //ON, OFF, TOGGLE
 	uint8_t n_led; //indica el número de led a controlar
 	uint8_t n_ciclos; //indica la cantidad de ciclos de encendido/apagado (parpadeo)
 	uint16_t periodo; //indica el tiempo de cada ciclo
-	} 
-}
-	//declaro una funcion void para el control de los leds
-	//esta funcion tiene como parametro un puntero al struct de leds
-	void control_leds (struct leds *my_leds)
-	{
+	};
+
+#define ON 1
+#define OFF 2
+#define TOGGLE 3
+
+/*==================[internal functions declaration]=========================*/
+//declaro una funcion void para el control de los leds
+//esta funcion tiene como parametro un puntero al struct de leds
+	void control_leds (struct leds *my_leds){
 		//esta funcion es la que sigue el diagrama de flujos
 		//la flecha es un operador de acceso a miembros de un puntero
 		//----MODO ON----
 		if (my_leds -> mode == ON){
-			if (my_leds-> n_led == 1){
-				LedOn(1);
+			if (my_leds -> n_led == LED_1){
+				LedOn(LED_1);
 			}
-			else if (my_leds-> n_led == 2){
-				LedOn(2);
+			else if (my_leds -> n_led == LED_2){
+				LedOn(LED_2);
 			}
-			else if (my_leds-> n_led == 3){
-				LedOn(3);
+			else if (my_leds -> n_led == LED_3){
+				LedOn(LED_3);
+			}
+		}
+		//----MODO OFF---
+		else if (my_leds -> mode == OFF){
+			if (my_leds -> n_led == LED_1){
+				LedOff(LED_1);
+			}
+			else if (my_leds -> n_led == LED_2){
+				LedOff(LED_2);
+			}
+			else if (my_leds -> n_led == LED_3){
+				LedOff(LED_3);
+			}
+		}
+		//----MODO TOGGLE---
+		else if (my_leds -> mode == TOGGLE){
+			for (int i = 0; i < my_leds -> n_ciclos; i++){
+				if (my_leds -> n_led == LED_1){
+					LedToggle(LED_1);
+				}
+				else if (my_leds -> n_led == LED_2){
+					LedToggle(LED_2);
+				}
+				else if (my_leds -> n_led == LED_3){
+					LedToggle(LED_3);
+				}
+			vTaskDelay(my_leds -> periodo / portTICK_PERIOD_MS);
 			}
 		}
 	}
+
+/*==================[external functions definition]==========================*/
+
+void app_main(void){
+	LedsInit();
+	struct leds mi_led;
+
+	mi_led.mode = TOGGLE;
+	mi_led.n_led = LED_1;
+	mi_led.n_ciclos = 10;
+	mi_led.periodo = 500; //estos son milisegundos
+	control_leds(&mi_led);
+}
+
+
 /*==================[end of file]============================================*/
